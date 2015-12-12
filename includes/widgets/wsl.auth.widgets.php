12c7a399cd3e8e30b6555b915cb6d1e298016019
thanhluu <thanhlt90@gmail.com>
2015-12-12 10:14:05 +0700

    change text & remove button

--- includes/widgets/wsl.auth.widgets.php
+++ includes/widgets/wsl.auth.widgets.php
@@ -227,7 +227,7 @@ function wsl_render_auth_widget( $args = array() )
 ?>
 
 		<a rel="nofollow" href="<?php echo $authenticate_url; ?>" title="<?php echo sprintf( _wsl__("Sign in with %s", 'wordpress-social-login'), $provider_name ) ?>" class="wp-social-login-provider wp-social-login-provider-<?php echo strtolower( $provider_id ); ?>" data-provider="<?php echo $provider_id ?>">
-			<?php if( $social_icon_set == 'none' ){ echo apply_filters( 'wsl_render_auth_widget_alter_provider_name', 'Connect with '.$provider_name ); } else { ?><img alt="<?php echo $provider_name ?>" title="<?php echo sprintf( _wsl__("Connect with %s", 'wordpress-social-login'), $provider_name ) ?>" src="<?php echo $assets_base_url . strtolower( $provider_id ) . '.png' ?>" /><?php } ?>
+			<?php if( $social_icon_set == 'none' ){ echo apply_filters( 'wsl_render_auth_widget_alter_provider_name', 'Sign In Using '.$provider_name ); } else { ?><img alt="<?php echo $provider_name ?>" title="<?php echo sprintf( _wsl__("Connect with %s", 'wordpress-social-login'), $provider_name ) ?>" src="<?php echo $assets_base_url . strtolower( $provider_id ) . '.png' ?>" /><?php } ?>
 
 		</a>
 <?php
@@ -237,9 +237,6 @@ function wsl_render_auth_widget( $args = array() )
 		}
 	}
 	?>
-	<a rel="nofollow" href="#" title="Sign in with DesignWall account" class="wp-social-login-provider wp-social-login-provider-dw" data-provider="DW">
-			Sign in with DW account
-		</a>
 	<?php
 
 	// no provider enabled?
